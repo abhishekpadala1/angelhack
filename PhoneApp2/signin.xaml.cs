48668cb703c5631ee3d50bafbc588d5d7f093d9b
abhishek padala <abhiabhi@live.com>
2014-05-11 13:26:12 -0700

    initial commit

---
+++ PhoneApp2/signin.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PhoneApp2
+{
+    public partial class signin : PhoneApplicationPage
+    {
+        public signin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
